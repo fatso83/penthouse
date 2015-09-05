665df6a0e4b13b2507f48eb2107df614457257ab
Carl-Erik Kopseng <carlerik@gmail.com>
2015-09-05 17:14:07 +0200

    Added Babel to dev deps to write ES6 tests
    Converted one test to see if it worked.

--- test/css-preformatter-test.js
+++ test/css-preformatter-test.js
@@ -1,36 +1,38 @@
-var path = require('path');
-var fs = require('fs');
-var read = fs.readFileSync;
-var css = require('css');
-var chai = require('chai');
-var should = chai.should();// extends Object.prototype (so ignore unused warnings)
+let path = require('path');
+let fs = require('fs');
+let read = fs.readFileSync;
+let css = require('css');
+let chai = require('chai');
+let should = chai.should();// extends Object.prototype (so ignore unused warnings)
 
 
-it('should preformat css (rm comments etc)', function (done) {
-    var cssPreformatCssFilePath = path.join(__dirname, 'static-server', 'preformat-css--remove.css'),
-        cssPreformatCss = read(cssPreformatCssFilePath).toString(),
-        cssPreformatter = require('../lib/phantomjs/css-preformatter.js');
+let cssPreformatCssFilePath = path.join(__dirname, 'static-server', 'preformat-css--remove.css'),
+	cssPreformatCss = read(cssPreformatCssFilePath).toString(),
+	cssPreformatter = require('../lib/phantomjs/css-preformatter.js');
 
-    var result = cssPreformatter(cssPreformatCss);
+describe('css preformatter', () => {
 
-    try {
-        var resultAst = css.parse(result);
-        var orgAst = css.parse(cssPreformatCss);
-        //with comments stripped out, fewer 'rules' (comments included) in AST
-        resultAst.stylesheet.rules.should.have.length.lessThan(orgAst.stylesheet.rules.length);
-        //but except for comments, (also inside declarations), everything should be the same
-        var orgAstRulesExceptComments = orgAst.stylesheet.rules.filter(function (rule) {
-            if (typeof rule.declarations !== "undefined") {
-                rule.declarations = rule.declarations.filter(function (declaration) {
-                    return declaration.type !== "comment"
-                })
-            }
-            return rule.type !== "comment";
-        });
-        orgAstRulesExceptComments.should.eql(resultAst.stylesheet.rules);
+	it('should preformat css (rm comments etc)', () => {
 
-        done();
-    } catch (ex) {
-        done(ex);
-    }
-});
+		let result = cssPreformatter(cssPreformatCss);
+
+		let resultAst = css.parse(result);
+		let orgAst = css.parse(cssPreformatCss);
+
+		//with comments stripped out, fewer 'rules' (comments included) in AST
+		resultAst.stylesheet.rules.should.have.length.lessThan(orgAst.stylesheet.rules.length);
+
+		//but except for comments, (also inside declarations), everything should be the same
+		let orgAstRulesExceptComments = orgAst.stylesheet.rules.filter((rule) => {
+			if (typeof rule.declarations !== "undefined") {
+				rule.declarations = rule.declarations.filter((declaration) => {
+					return declaration.type !== "comment"
+				})
+			}
+			return rule.type !== "comment";
+		});
+
+		orgAstRulesExceptComments.should.eql(resultAst.stylesheet.rules);
+	});
+
+})
